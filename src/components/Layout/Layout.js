509dbb3d656a291b2a6c34e99326a29fd56da0a9
Chris Haralson <ocarmike@sonic.com>
2018-03-03 19:47:26 -0800

    layout complete

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import Aux from '../../hoc/Aux';
+
 const layout = ( props ) => (
+    <Aux>
     <div>Toolbar, SideDrqwer, Backdrop</div>
     <main>
         {props.children}
     </main>
+    </Aux>
 );
+
+export default layout;
