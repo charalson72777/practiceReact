82bcc38599479c38d7b868e16feb948ab038acb4
Chris Haralson <ocarmike@sonic.com>
2018-03-06 18:43:42 -0800

    sidedrwr

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem>Burger Builder</NavigationItem>
-        <NavigationItem>Checkout</NavigationItem>
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
     </ul>
 );
 
